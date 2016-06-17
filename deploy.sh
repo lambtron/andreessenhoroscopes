1a50394f91b8b265c84ae655c23a7782cd68c73d
lambtron <andyjiang@gmail.com>
2016-06-17 15:48:33 -0700

    Updated site

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,10 @@ if [[ $(git name-rev --name-only HEAD) != "master" ]]; then
 fi
 
 # Validate that there are no uncommitted files.
-if [[ $(git status 2> /dev/null | tail -n1) == "nothing to commit, working directory clean" ]]; then
-  echo "Working directory clean. No changes were made."
-  exit 1
-fi
+# if [[ $(git status 2> /dev/null | tail -n1) == "nothing to commit, working directory clean" ]]; then
+#   echo "Working directory clean. No changes were made."
+#   exit 1
+# fi
 
 set -x
 
